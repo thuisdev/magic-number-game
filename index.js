8ab879fcc4558997a029205f7eb5dd9064af80d4
Adrian <adrian.thuis04@gmail.com>
2026-05-10 16:31:16 +0200

    cleaned up the codebase

--- index.js
+++ index.js
@@ -16,31 +16,31 @@ let reset = document.getElementById("btn-reset")
 
 let attempts = 0
 
-guess.addEventListener("click", function(){
+guess.addEventListener("click", function () {
     let guess = input.value;
- 
-    if(input.value === ""){
+
+    if (input.value === "") {
         alert("Please type in a number")
         return
     }
 
     [high, low, congrats].forEach(el => el.style.display = "none")
 
-    if (guess > random){
-        high.style.display="block"
+    if (guess > random) {
+        high.style.display = "block"
     }
-    else if (guess < random){
-        low.style.display="block"
+    else if (guess < random) {
+        low.style.display = "block"
     }
     else {
-        congrats.style.display="block"
+        congrats.style.display = "block"
     }
 
     attempts++
-    counter.innerHTML ="Attempts:" + attempts
+    counter.innerHTML = "Attempts:" + attempts
 })
 
-reset.addEventListener("click", function(){
+reset.addEventListener("click", function () {
     attempts = 0
     counter.innerHTML = "Attempts:" + attempts
     input.value = ""
